c3c05df8f8e0d24dd0ac7b5360d9925d8cb664d1
orhankirmiziny <kirmizi.orhan@gmail.com>
2023-04-05 16:17:46 -0400

    Us02 is done -- added wait for avoiding issues

--- src/test/java/com/library/steps/US02_Orhan_Neli_StepDefs.java
+++ src/test/java/com/library/steps/US02_Orhan_Neli_StepDefs.java
@@ -2,6 +2,7 @@ package com.library.steps;
 
 import com.library.pages.DashBoardPage;
 import com.library.pages.LoginPage;
+import com.library.utility.BrowserUtil;
 import com.library.utility.DB_Util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,7 +10,6 @@ import org.junit.Assert;
 
 public class US02_Orhan_Neli_StepDefs {
 
-    LoginPage loginPage;
     DashBoardPage dashBoardPage;
 
     String actualBorrowedBookNumber;
@@ -18,19 +18,20 @@ public class US02_Orhan_Neli_StepDefs {
     public void the_librarian_gets_borrowed_books_number() {
 
         //BrowserUtil.waitForVisibility(dashBoardPage.usersNumber,3);
-
+        BrowserUtil.waitFor(2);
         dashBoardPage = new DashBoardPage();
         //OPT 1
         actualBorrowedBookNumber = dashBoardPage.borrowedBooksNumber.getText();
         System.out.println("actualBorrowedBookNumber = " + actualBorrowedBookNumber);
 
         //OPT 2
-        System.out.println("dashBoardPage.getModuleCount(\"Borrowed Books\") = " + dashBoardPage.getModuleCount("Borrowed Books"));
+        //System.out.println("dashBoardPage.getModuleCount(\"Borrowed Books\") = " + dashBoardPage.getModuleCount("Borrowed Books"));
     }
 
     @Then("borrowed books number information must match with DB")
     public void borrowed_books_number_information_must_match_with_db() {
 
+        BrowserUtil.waitFor(2);
         String query="select count(*) from book_borrow where is_returned=0";
 
         DB_Util.runQuery(query);
